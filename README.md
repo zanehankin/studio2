a12c2ab14b203764fbd654d744b5815625bb3481
Zane Hankin <zanehankin@wustl.edu>
2023-08-30 13:01:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CSE 332 Studio Base
+Repository for submitting studio work
+
+# Responses:
+Students:
+1. John Doe <john.doe@wustl.edu>
+2. 
+3. 
+4. 
+
+
+Question Answers:
+2. 
+
---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+/**
+ * Main.cpp
+ * CSE 332S
+ * Studio Num:
+ * Author:
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
